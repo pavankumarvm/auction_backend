cadf9814f7aa7f0fbd7aec6f1733cec880330bbe
pavankumarvm <pavankumarmaurya1999@gmail.com>
2021-12-30 10:56:31 +0530

    cors removed

--- auction_system/settings.py
+++ auction_system/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -64,12 +63,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://vitualauction.herokuapp.com',
-    'http://virtualauction-api.herokuapp.com'
-)
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:3000',
+#     'http://localhost:8000',
+#     'http://vitualauction.herokuapp.com',
+#     'http://virtualauction-api.herokuapp.com'
+# )
 
 ROOT_URLCONF = 'auction_system.urls'
 
